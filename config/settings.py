1341d31e5f16d0d1e75909d962725887012c90a0
t23-ward <thomas7.ward@live.uwe.ac.uk>
2026-03-05 10:07:23 +0000

    Update settings.py
    Updated DATABASES so it works with docker

--- config/settings.py
+++ config/settings.py
@@ -52,10 +52,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
