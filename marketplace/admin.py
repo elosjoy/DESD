09189cf093039a9063b0d8b415134feb290106b4
t23-ward <thomas7.ward@live.uwe.ac.uk>
2026-03-12 21:43:52 +0000

    Added search function

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -11,4 +11,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'producer', 'price', 'stock_quantity', 'availability_status')
     list_filter = ('category', 'availability_status', 'producer')
     search_fields = ('name', 'producer__producer_name')
-    readonly_fields = ('producer',)
+
+    def get_readonly_fields(self, request, obj=None):
+        # Allow producer assignment on create, lock it once created.
+        if obj:
+            return ('producer',)
+        return ()
