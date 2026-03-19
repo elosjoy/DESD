ef9ab4f7ab3145783f470ab1f382a5b0e657b338
t23-ward <thomas7.ward@live.uwe.ac.uk>
2026-03-19 16:25:52 +0000

    Update admin.py

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -11,9 +11,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'producer', 'price', 'stock_quantity', 'availability_status')
     list_filter = ('category', 'availability_status', 'producer')
     search_fields = ('name', 'producer__producer_name')
-
-    def get_readonly_fields(self, request, obj=None):
-        # Allow producer assignment on create, lock it once created.
-        if obj:
-            return ('producer',)
-        return ()
+    readonly_fields = ('producer',)
